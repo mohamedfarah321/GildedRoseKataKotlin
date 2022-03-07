e3247f5fb6a838f5b4c578144704bb3be7d2ce35
Mohamed Farah <mohamed.farah2@ing.com>
2022-03-07 11:23:56 +0100

    further simplification for the iteration

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,56 +4,61 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
 
-        for (i in items.indices) {
-            if (!(items[i].isAgedBrie()) && !(items[i].isBackStagePass())) {
-                if (items[i].quality > 0) {
-                    if (!(items[i].isSulfuras())) {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+        items.forEach { item ->
+            updateQualityPerItem(item)
+        }
+    }
 
-                    if ((items[i].isBackStagePass())) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
+    private fun updateQualityPerItem(item: Item) {
+        if (!item.isAgedBrie() && !(item.isBackStagePass())) {
+            if (item.quality > 0) {
+                if (!(item.isSulfuras())) {
+                    item.quality = item.quality - 1
                 }
             }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
 
-            if (!(items[i]).isSulfuras()) {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!(items[i]).isAgedBrie()) {
-                    if (!(items[i].isBackStagePass())) {
-                        if (items[i].quality > 0) {
-                            if (!(items[i].isSulfuras())) {
-                                items[i].quality = items[i].quality - 1
-                            }
+                if ((item.isBackStagePass())) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
                     }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
+                        }
                     }
                 }
             }
         }
+
+        if (!item.isSulfuras()) {
+            item.sellIn = item.sellIn - 1
+        }
+
+        if (item.sellIn < 0) {
+            if (!item.isAgedBrie()) {
+                if (!item.isBackStagePass()) {
+                    if (item.quality > 0) {
+                        if (!(item.isSulfuras())) {
+                            item.quality = item.quality - 1
+                        }
+                    }
+                } else {
+                    item.quality = item.quality - item.quality
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
+                }
+            }
+        }
     }
+
 
     private fun Item.isAgedBrie() = name.contains(AGED_BRIE, ignoreCase = true)
     private fun Item.isSulfuras() = name.contains(SULFURAS, ignoreCase = true)
